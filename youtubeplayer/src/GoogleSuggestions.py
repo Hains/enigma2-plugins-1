b6fa31e4393c33b637ed31ae1db85afd1d7bcc18
Hains <hainsvdbosch@ziggo.nl>
2020-08-16 10:51:01 +0200

    GoogleSuggestions.py: exec is a function
    | ***   File "/home/hains/python3/openvision-development-platform/build/tmp/work/ustym4kpro-oe-linux-gnueabi/enigma2-plugins/g
    itAUTOINC+2c399febf6-r0/git/youtubeplayer/src/GoogleSuggestions.py", line 64
    |     exec data
    |          ^
    | SyntaxError: Missing parentheses in call to 'exec'

--- youtubeplayer/src/GoogleSuggestions.py
+++ youtubeplayer/src/GoogleSuggestions.py
@@ -61,7 +61,7 @@ class GoogleSuggestions():
 				else:
 					if response.status == 200:
 						data = response.read()
-						exec data
+						exec(data)
 					else:
 						self.callback(None)
 			self.conn.close()

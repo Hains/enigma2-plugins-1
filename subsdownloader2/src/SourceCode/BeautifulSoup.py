9be9842bf1a2e3b5d1da28b50ce66b35d3860b24
Hains <hainsvdbosch@ziggo.nl>
2020-09-20 13:35:54 +0200

    BeautifulSoup.py: from __future__ imports must occur at the beginning of the file

--- subsdownloader2/src/SourceCode/BeautifulSoup.py
+++ subsdownloader2/src/SourceCode/BeautifulSoup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+from __future__ import generators
 """Beautiful Soup
 Elixir and Tonic
 "The Screen-Scraper's Friend"
@@ -79,7 +80,6 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE, DAMMIT.
 
 """
-from __future__ import generators
 
 __author__ = "Leonard Richardson (leonardr@segfault.org)"
 __version__ = "3.2.0"
